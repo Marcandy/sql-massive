ea882dff6f31c1af35998c4bafe6dbad5077d643
marcandy <marcandy.nj@gmail.com>
2017-01-11 15:11:15 -0700

    massive

---
+++ track.js
@@ -0,0 +1,11 @@
+CREATE TABLE products (
+Id SERIAL PRIMARY KEY,
+Name text,
+Description text,
+Price text,
+Imageurl  text
+);
+
+INSERT INTO products (Name, Description, Price, Imageurl) VALUES
+ ('Soda', 'its a drink', 15, 'dta tada'),
+ ('Burrito','food to eat', '5', 'to infinity'
